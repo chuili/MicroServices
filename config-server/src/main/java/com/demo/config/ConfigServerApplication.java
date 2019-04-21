33bc358003ed872b4422f5887796ac0852507452
chuili <chuili.lim@hotmail.com>
2019-04-21 20:20:17 +0800

    Rename config server directory and correct the properties file name

--- config-server/src/main/java/com/demo/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/demo/config/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.demo.configserver;
+package com.demo.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- config-server/src/test/java/com/demo/config/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/demo/config/ConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.demo.configserver;
+package com.demo.config;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
