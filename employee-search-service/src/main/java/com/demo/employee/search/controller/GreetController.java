925dbf055b10576cf08954d039920d74ab36d9e2
chuili <chuili.lim@hotmail.com>
2019-04-23 00:34:07 +0800

    Rename config properties directory and convert controller class to interface

--- employee-search-service/src/main/java/com/demo/employee/search/controller/GreetController.java
+++ employee-search-service/src/main/java/com/demo/employee/search/controller/GreetController.java
@@ -1,18 +1,12 @@
 package com.demo.employee.search.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
-@RestController
-public class GreetController {
-    @Value("${name}")
-    private String name;
+public interface GreetController {
 
     @RequestMapping("/greeting")
-    public String greet() {
-        return String.format("Hello %s", name);
-    }
+    public String greet();
+
 }
